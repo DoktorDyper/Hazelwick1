934b8c579fc0a0c9a0db2e3e137875b01c600b61
DoktorDyper <isabellavogli@aol.com>
2026-09-23 12:22:16 +0000

    23/09/2026

---
+++ Isabella/Leap Years and days in a month.py
@@ -0,0 +1,13 @@
+year = int(input("Enter a year: "))
+
+fourRemainder = year // 4
+oneHundredRemainder = year // 100
+fourHundredRemainder = year // 400
+
+if (fourRemainder != 0 and oneHundredRemainder != 0) or fourHundredRemainder != 0:
+    print(year "is not a Leap Year")
+
+elif
+
+endif 
+
